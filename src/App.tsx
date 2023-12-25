c1e923d7f103b65540f979bc1c6d974887874789
faran <hossinifaran@gmail.com>
2023-12-25 20:25:10 +0330

    compeleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {Section} from "./section";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <main className={"sections-container"}>
+
+            <div className="home-sections">
+                <Section id={1} texts={["2021", "Fashion & Accessories", "Branding","Website"]} title={"NEARA"}
+                         imageUrl={"https://assets.website-files.com/6266fd2c922d57e602d9502d/6266fd2c922d571ab9d9502f_neara-cover-home.jpg"}/>
+                <Section id={2} texts={["2021", "Food & Beverage", "Branding","Packaging"]} title={"I, THE QUEEN"}
+                         imageUrl={"https://assets.website-files.com/6266fd2c922d57e602d9502d/6266fd2c922d57b75fd95030_scepter-cover-2.jpg"}/>
+                <Section id={3} texts={["2021", "Real Estate", "Branding"]} title={"THE RADD"}
+                         imageUrl={"https://assets.website-files.com/6266fd2c922d57e602d9502d/6266fd2c922d57d06dd95033_theradd-cover-home.jpg"}/>
+                <Section id={4} texts={["2021", "Hospitality", "Branding","Website"]} title={"FUENTE REAL"}
+                         imageUrl={"https://assets.website-files.com/6266fd2c922d57e602d9502d/6266fd2c922d57670dd95031_fuentereal-cover-home.jpg"}/>
+                <Section id={5} texts={["2021", "Real Estate", "Branding","UI/UX"]} title={"FENWAY CENTER"}
+                         imageUrl={"https://assets.website-files.com/6266fd2c922d57e602d9502d/6266fd2c922d570336d95034_fenwaycenter-cover-home.jpg"}/>
+                <Section id={6} texts={["2021", "Professional Services", "Branding","UI/UX"]} title={"APOSTLE DIGITAL"}
+                         imageUrl={"https://assets.website-files.com/6266fd2c922d57e602d9502d/6266fd2c922d57f61dd95032_apostle-cover-home.jpg"}/>
+            </div>
+            <div className={"lg:flex hidden fixed bottom-0 end-0 page-number px-8"}>
+                <span className={"text-white me-8"}>
+                    <span>1</span>
+                    /
+                    <span>6</span>
+                </span>
+                <span className={"page-number__arrow rotate-180 text-white"}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="2.5vw" viewBox="0 0 9.414 13.6">
+                        <g id="Group_16" data-name="Group 16" transform="translate(-1.293 1.5)">
+                            <line id="Line_1" data-name="Line 1" y1="13" transform="translate(6 -1.5)" fill="none"
+                                  stroke="currentColor" strokeLinejoin="round" strokeWidth="1.3"></line>
+                            <path id="Path_1320" data-name="Path 1320" d="M10,7.5l-4,4-4-4" fill="none"
+                                  stroke="currentColor" strokeWidth="1.3"></path>
+                        </g>
+                    </svg>
+                </span>
+            </div>
+        </main>
+    )
 }
 
 export default App;
---
+++ src/section.tsx
@@ -0,0 +1,111 @@
+import React, {useRef} from "react";
+import {useGSAP} from "@gsap/react";
+import {gsap} from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+interface SectionProps {
+    imageUrl: string,
+    title: string,
+    texts: string[],
+    id: number
+}
+
+export const Section: React.FC<SectionProps> = (T) => {
+    const {id, imageUrl, title, texts} = T;
+    const section: React.MutableRefObject<HTMLElement | null> = useRef(null)
+    const sectionBackground: React.MutableRefObject<HTMLImageElement | null> = useRef(null)
+    useGSAP(() => {
+        const Entering = () => {
+            if (id !== 1) {
+                gsap.timeline().to(`.image-wrapper-${id}`, {
+                    clipPath: "polygon(0px 0px, 100% 0%, 100% 100%, 0% 100%)",
+                    transform: "rotate(0)",
+                    duration: .5
+                }).to(`.section-${id}-texts`, {
+                    opacity: 1,
+                    transform: "translate(0)",
+                    stagger: .2
+                }, ">").to(`.section-${id}-view-btn`, {
+                    opacity: 1,
+                }, "<").to(`.section-${id}-title`, {
+                    opacity: 1,
+                    transform: "translate(0)",
+                    stagger: .2
+                }, ">")
+            } else {
+                gsap.timeline().to(`.section-${id}-texts`, {
+                    opacity: 1,
+                    transform: "translate(0)",
+                    stagger: .2
+                }).to(`.section-${id}-view-btn`, {
+                    opacity: 1,
+                }, "<").to(`.section-${id}-title`, {
+                    opacity: 1,
+                    transform: "translate(0)",
+                    stagger: .2
+                }, ">")
+            }
+        }
+
+        const Leaving = (isBackLeave: boolean) => {
+            if (id !== 1 && isBackLeave) {
+                gsap.timeline().to(`.image-wrapper-${id}`, {
+                    clipPath: "polygon(50% 50%, 50% 50%, 50% 50%, 50% 50%)",
+                    transform: "rotate(-50deg)",
+                    duration: .5
+                })
+            }
+            gsap.killTweensOf(`.section-${id}-texts`, {opacity: 0, transform: "translate(0,1rem)"})
+            gsap.killTweensOf(`.section-${id}-view-btn`, {opacity: 0, transform: "translate(0,1rem)"})
+            gsap.killTweensOf(`.section-${id}-title`, {opacity: 0, transform: "translate(0,1rem)"})
+            gsap.set(`.section-${id}-title`, {opacity: 0, transform: "translate(0,1rem)"})
+            gsap.set(`.section-${id}-texts`, {opacity: 0})
+            gsap.set(`.section-${id}-view-btn`, {opacity: 0})
+        }
+
+        gsap.to(sectionBackground.current, {
+            scale: "1.2 1.2",
+            scrollTrigger: {
+                trigger: ".sections-container",
+                start: () => {
+                    return (id - 1) * (document.querySelector(`.section-${id}`) as HTMLElement).clientHeight * 2
+                },
+                end: () => {
+                    return (id * ((document.querySelector(`.section-${id}`) as HTMLElement).clientHeight * 2))
+                },
+                pin: true,
+                scrub: 3,
+                onEnter: () => Entering(),
+                onEnterBack: () => Entering(),
+                onLeaveBack: () => Leaving(true),
+                onLeave: () => Leaving(false),
+            }
+        })
+    })
+    return (
+        <section key={id} style={{zIndex: id}} ref={section}
+                 className={`section-container absolute section-${id} w-screen px-8`}>
+            <figure style={{clipPath: id !== 1 ? "polygon(50% 50%, 50% 50%, 50% 50%, 50% 50%)" : "none"}}
+                    className={`${id % 2 === 0 && id !== 1 ? "rotate-45" : id !== 1 ? "-rotate-45" : ""} w-full inset-0 h-full absolute image-wrapper-${id}`}>
+                <img alt={"background"} ref={sectionBackground}
+                     className={`w-full object-cover section-container__image image-${id}`}
+                     src={imageUrl}/>
+            </figure>
+            <div className={"absolute top-1/2 -translate-y-1/2"}>
+                {texts.map((text, index) => {
+                    return <h2 key={index}
+                               className={`${id !== 1 ? "translate-y-4 opacity-0" : ""} section-container__text section-${id}-texts  text-white`}>{text}</h2>;
+                })}
+            </div>
+
+            <div className={"h-full w-full flex justify-end items-center"}>
+                <button
+                    className={`${id !== 1 ? "translate-y-4 opacity-0" : ""} section-container__view-btn section-${id}-view-btn  text-white border-white border-solid border-2 rounded-full`}>VIEW
+                </button>
+            </div>
+            <h1 className={`${id !== 1 ? "translate-y-4 opacity-0" : ""} section-container__title section-${id}-title absolute bottom-0 text-white`}>{title}</h1>
+        </section>
+    );
+}
